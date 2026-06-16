3de7e6328957865638d355c87990a8a1c2528b06
«SergeyOkey» <sergeyborisov_1@vk.ru>
2026-06-16 21:20:56 +0300

    YandexMetrika fix

--- plugins/metrika.client.js
+++ plugins/metrika.client.js
@@ -1,22 +1,26 @@
+// plugins/metrika.client.js
 export default defineNuxtPlugin(() => {
-  if (process.client) {
-    window.ym = window.ym || function() {
-      (window.ym.a = window.ym.a || []).push(arguments)
-    }
-    window.ym.l = 1 * new Date()
-
-    const script = document.createElement('script')
-    script.async = true
-    script.src = 'https://mc.yandex.ru/metrika/tag.js?id=109905993'
-    document.head.appendChild(script)
-
-    setTimeout(() => {
-      ym(109905993, 'init', {
-        clickmap: true,
-        trackLinks: true,
-        accurateTrackBounce: true,
-        webvisor: true,
-      })
-    }, 0)
+  // Код выполняется только на клиенте (благодаря .client в имени файла)
+  
+  // Инициализация счётчика
+  window.ym = window.ym || function() {
+    (window.ym.a = window.ym.a || []).push(arguments)
   }
+  window.ym.l = 1 * new Date()
+
+  // Загрузка скрипта метрики
+  const script = document.createElement('script')
+  script.async = true
+  script.src = 'https://mc.yandex.ru/metrika/tag.js?id=109905993'
+  document.head.appendChild(script)
+
+  // Инициализация после загрузки
+  setTimeout(() => {
+    ym(109905993, 'init', {
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+      webvisor: true
+    })
+  }, 100)
 })
