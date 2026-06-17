a9f2439f680b68cf34d1e73e01359554a4a6f90d
Sergey-Okey <sergeyborisov_1@vk.ru>
2026-06-17 08:51:15 +0300

    Yandex metrika rebags

--- app.vue
+++ app.vue
@@ -30,6 +30,42 @@ import { getBackendFetchOptions, getBackendUrl } from '~/utils/backend'
 import { useSyncStatus } from '~/composables/useSyncStatus'
 import { browserLog } from '~/utils/browserLog'
 
+useHead({
+  script: [
+    {
+      key: 'yandex-metrika',
+      type: 'text/javascript',
+      innerHTML: `
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { return; }
+          }
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109905993', 'ym');
+
+        ym(109905993, 'init', {
+          ssr: true,
+          webvisor: true,
+          clickmap: true,
+          ecommerce: "dataLayer",
+          referrer: document.referrer,
+          url: location.href,
+          accurateTrackBounce: true,
+          trackLinks: true
+        });
+      `,
+    },
+  ],
+  noscript: [
+    {
+      key: 'yandex-metrika-noscript',
+      innerHTML: `<div><img src="https://mc.yandex.ru/watch/109905993" style="position:absolute; left:-9999px;" alt="" /></div>`,
+    },
+  ],
+})
+
 const tasksStore = useTasksStore()
 const settingsStore = useSettingsStore()
 const userStore = useUserStore()
@@ -91,37 +127,38 @@ onMounted(async () => {
   if (accessStore.isDemo) {
     syncStatus.setState('local')
     browserLog.info('sync', 'Sync disabled in demo mode')
-  } else try {
-    syncStatus.setState('syncing')
-    browserLog.info('sync', 'Requesting initial sync')
-    const data = await backendFetch<SyncResponse>(syncEndpoint, {
-      query: { userId: userId.value },
-      ...getBackendFetchOptions(),
-    })
-    if (data.user) userStore.$patch(data.user)
-    if (data.tasks) tasksStore.$patch(data.tasks)
-    if (data.branches?.branches) {
-      branchesStore.$patch({ branches: data.branches.branches })
+  } else
+    try {
+      syncStatus.setState('syncing')
+      browserLog.info('sync', 'Requesting initial sync')
+      const data = await backendFetch<SyncResponse>(syncEndpoint, {
+        query: { userId: userId.value },
+        ...getBackendFetchOptions(),
+      })
+      if (data.user) userStore.$patch(data.user)
+      if (data.tasks) tasksStore.$patch(data.tasks)
+      if (data.branches?.branches) {
+        branchesStore.$patch({ branches: data.branches.branches })
+      }
+      if (data.branches?.edges) branchesStore.replaceEdges(data.branches.edges)
+      if (data.rewards) rewardsStore.$patch(data.rewards)
+      if (data.tags) {
+        tagsStore.$patch(data.tags)
+        tagsStore.normalizeTags(tasksStore.tasks)
+      }
+      if (data.ui) uiStore.$patch(data.ui)
+      if (data.settings) {
+        settingsStore.$patch(data.settings)
+        settingsStore.applyRuntimeSettings()
+      }
+      syncStatus.setState(authStore.authMode === 'local' ? 'local' : 'synced')
+      browserLog.info('sync', 'Initial sync completed', {
+        mode: authStore.authMode,
+      })
+    } catch {
+      syncStatus.setState(navigator.onLine ? 'error' : 'offline')
+      browserLog.warn('sync', 'Cloud sync unavailable, using local data')
     }
-    if (data.branches?.edges) branchesStore.replaceEdges(data.branches.edges)
-    if (data.rewards) rewardsStore.$patch(data.rewards)
-    if (data.tags) {
-      tagsStore.$patch(data.tags)
-      tagsStore.normalizeTags(tasksStore.tasks)
-    }
-    if (data.ui) uiStore.$patch(data.ui)
-    if (data.settings) {
-      settingsStore.$patch(data.settings)
-      settingsStore.applyRuntimeSettings()
-    }
-    syncStatus.setState(authStore.authMode === 'local' ? 'local' : 'synced')
-    browserLog.info('sync', 'Initial sync completed', {
-      mode: authStore.authMode,
-    })
-  } catch {
-    syncStatus.setState(navigator.onLine ? 'error' : 'offline')
-    browserLog.warn('sync', 'Cloud sync unavailable, using local data')
-  }
 
   tasksStore.resetDailyTasks()
   tagsStore.normalizeTags(tasksStore.tasks)
@@ -129,11 +166,19 @@ onMounted(async () => {
   window.addEventListener('beforeunload', autoBackupOnUnload)
 
   // Store subscriptions capture edits and nested mutations without broad deep watchers.
-  ;[userStore, tasksStore, branchesStore, rewardsStore, tagsStore, uiStore, settingsStore].forEach(
-    (store) => {
-      syncUnsubscribers.push(store.$subscribe(() => syncToCloud(), { detached: true }))
-    }
-  )
+  ;[
+    userStore,
+    tasksStore,
+    branchesStore,
+    rewardsStore,
+    tagsStore,
+    uiStore,
+    settingsStore,
+  ].forEach((store) => {
+    syncUnsubscribers.push(
+      store.$subscribe(() => syncToCloud(), { detached: true })
+    )
+  })
 })
 
 const syncToCloud = useDebounceFn(async () => {
@@ -215,4 +260,3 @@ function autoBackupOnUnload() {
   settingsStore.recordBackup()
 }
 </script>
-
